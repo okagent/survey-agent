119c03de7df4a19ac992bcb5e185f3861d7f7f7e
WeiShi-9 <1392271165@qq.com>
2023-12-17 18:15:25 +0800

    update query functions

--- code/query_func.py
+++ code/query_func.py
@@ -1,3 +1,31 @@
+
+from paper_func import *
+from llm_tools import *
+
+
+def merge_chunk_responses(responses, question):    
+    #merge all the chunk answers if needed
+        # f = open(f"../prompts/merge_answer.txt", "r")
+        # all_res = responses
+        # merge_template = f.read()
+        # res_l=0
+        # while len(all_res)>1:
+        #     res_l+=1
+        #     responses_batch, ids = get_batches(all_res, prompt_text=merge_template)
+        #     batch_prompts=[]
+        #     for res, id in zip(responses_batch, ids):
+        #         prompt = merge_template.format(responses=format_res(res), question=self.query)
+        #         batch_prompts.append(prompt)
+        #     all_res = predict(batch_prompts)
+        # if res_l>1:
+        #     print("all res level: ", res_l)
+
+
+        # merged_response = all_res[0]
+            
+        # return merged_response
+        pass
+
 def query_area_papers(paper_list_name, question):
     """
     Queries a large collection of papers to find an answer to a specific question.
@@ -7,10 +35,49 @@ def query_area_papers(paper_list_name, question):
     question (str): The question to be queried.
 
     Returns:
-    list: A list of tuples containing the answer, the source paragraph, and the source paper.
+    list: A list of dictionaries containing the answer, the source paragraph, and the source paper.
     """
-    # TODO: Implement the logic to query a large collection of papers
-    pass
+    # Assume we can get a list of paper names
+    paper_list = get_paperlist_by_name(paper_list_name)
+    
+    # chunk the large collection of papers into chunks
+    chunk_list=[]
+    for p in paper_list:
+        content = get_paper_content(p ,mode="short")
+        chunks = get_chunks(content)
+        for c in chunks:
+            chunk_list.append((p,c))
+            
+    #check for relevant chunks => paper name and paragraph content
+    f = open(f"../prompts/check_for_related.txt", "r")
+    check_for_related = f.read()
+    prompts=[]
+    for d in chunk_list:
+        prompts.append(check_for_related.format(title=d[0], content=d[1]))
+    res = small_model_predict(prompts)
+    
+    #parse for references, answers
+    answer_and_source=[]
+    f = open(f"../prompts/collect_answer_from_chunk.txt", "r")
+    query_chunk = f.read()
+    query_chunk_prompts=[]
+    for i,j in zip(res,chunk_list):
+        if "yes" in i.lower():
+            leave={}
+            leave['source_content'] = j[1]
+            leave['source_paper'] = j[0]
+            answer_and_source.append(leave)
+            query_chunk_prompts.append(query_chunk.format(chunk=j[1], question=question))
+    answers = small_model_predict(query_chunk_prompts)
+    for i,j in zip(answers, answer_and_source):
+        j['answer'] = i
+        
+    #merge for final complete answer if needed
+    
+    return answer_and_source
+    
+    
+            
 
 def query_individual_papers(paper_list_name, question):
     """
@@ -23,5 +90,22 @@ def query_individual_papers(paper_list_name, question):
     Returns:
     list: A list of tuples containing the answer, the source paragraph, and the source paper.
     """
-    # TODO: Implement the logic to query a small collection of papers
-    pass
+    # Assume we can get a list of paper names
+    paper_list = get_paperlist_by_name(paper_list_name)
+    
+    # Assume we can put all the content of one paper into the model
+    f = open(f"../prompts/collect_answer_from_whole_paper.txt", "r")
+    query_paper = f.read()
+    answer_with_source=[]
+    for p in paper_list:
+        paper_content = get_paper_content(p, mode="full")
+        prompt = query_paper.format(content=paper_content, question=question)
+        res = gpt_4_predict(prompt)
+        leave = {
+            "answer": res,
+            "source_paper": p,
+            "source_content": paper_content
+        }
+        answer_with_source.append(leave)
+    
+    return answer_with_source
