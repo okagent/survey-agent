6d5d25ca64f45f6c0512b93e79e2a5d794b2273f
WeiShi-9 <1392271165@qq.com>
2023-12-25 15:32:15 +0800

    small modification

--- code/utils.py
+++ code/utils.py
@@ -18,7 +18,7 @@ from contextlib import contextmanager
 config_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'config.json')
 with open(config_path, 'r') as f:
     config = json.load(f)
-import pdb; pdb.set_trace()
+# import pdb; pdb.set_trace()
 
 
 logger = logging.getLogger(__name__)
